f4da52494ea884ce0050d15653d7e0a165536e8c
douqin <dagxuanlam@gmail.com>
2025-05-16 15:01:23 +0700

    fix: authentication for socketio

--- src/socketio/socket.ts
+++ src/socketio/socket.ts
@@ -3,7 +3,21 @@ import { io, Socket } from "socket.io-client";
 let connection: Socket | null = null;
 
 export function initializeSocket() {
-  if (!connection) connection = io(import.meta.env.VITE_URL_BACKEND + "/");
+  if (!connection)
+    connection = io(import.meta.env.VITE_URL_BACKEND + "/", {
+      autoConnect: true,
+      reconnectionAttempts: 5,
+      auth: (cb) => {
+        const authorization = "Bearer " + localStorage.getItem("accessToken");
+        if (authorization) {
+          cb({ authorization });
+        } else {
+          cb({ authorization: null });
+        }
+      },
+      reconnectionDelay: 1000,
+      timeout: 20000,
+    });
 
   connection.on("disconnect", (reason, details) => {
     console.error("[Socket.IO] Disconnected:", reason, details);
@@ -12,7 +26,7 @@ export function initializeSocket() {
     console.log("[Socket.IO] Connected");
   });
   connection.on("connect_error", (error) => {
-    console.log("[Socket.IO] Connected:", error);
+    console.log("[Socket.IO] Error when try to connect:", error);
   });
   connection.on("reconnect_attempt", () => {
     console.warn("[Socket.IO] Reconnecting attempt");
